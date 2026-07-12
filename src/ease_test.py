07f091fcf66c8f73046cbc3175b7b0cda9fc079e
Ziegenhainy <hawipru@gmail.com>
2026-07-12 16:09:39 +0200

    incredibly trippy and cool thingy

--- src/ease_test.py
+++ src/ease_test.py
@@ -5,10 +5,10 @@ mov = TUIMov(100,30, "examples/ease_test.tuimov")
 for i in range(10):
     box = BoxObject(y=i*3, width=6)
     mov.add_object(box)
-    box_tween = box.move_x_tween(90, TUITime(nanoseconds=BILLION//(100+i)*100), sin_ease)
-    for _ in range(100+i):
-        mov.add_tween(box_tween, move_cursor=True)
-        mov.add_action(TUIAction(box.move, (-90,)))
+    box_tween = box.move_x_tween(90, TUITime(nanoseconds=BILLION//(100+i*2)*100), sin_ease)
+    box_tween_2 = box.move_x_tween(-90, TUITime(nanoseconds=BILLION//(100+i*2)*100), sin_ease)
+    for j in range(100+i*2):
+        mov.add_tween(box_tween if j%2 == 0 else box_tween_2, move_cursor=True)
     mov.cursor = ZERO_TIME
 
 mov.render()
